8a682280e15e738045446cf42abe4c48f3640df2
lilli <lillic@gmail.com>
2024-05-05 13:10:53 -0700

    Update convert_images.py based on discussion in examine_images notebook
    All images convert correctly now.
    Print statements indicate completion and which images required separate processing steps.
    Cleaned up and simplified code overall based on improved understanding of images and pillow library.

--- scripts/convert-images.py
+++ scripts/convert-images.py
@@ -1,75 +1,60 @@
 #!/usr/bin/python3
-'''convert b&w TIFF icon images to JPEG, resizing, rotating, and accounting for transparency'''
-#using black or white masks as appropriate, since JPEG doesn't support transparency
+'''convert set of b&w TIFF icon images to JPEG, resizing, rotating, and accounting for transparency'''
 
-from PIL import Image
+from PIL import Image, ImageOps
 from glob import glob
 import re
 from os import path
 import pathlib
 
 def has_bad_extrema(img):
-    #b&w images are either flat (an error) or suspicious if they don't use the full pixel depth
-    #assuming image modes are either 8-bit or binary
+    #Compare image pixel extrema to their full mode range
+    #flag if these B&W images don't use exactly the full depth
+    #Note: assumes images are 8-bit or binary
     mode = img.mode
     extra = img.getextrema()
     full = 255
-    depth = 0
-    dmin = full
-    dmax = 0
 
-    if 'A' in mode:
-        #has additional transparency layer
-        #check that at least one layer uses full depth
-        #get overall min/max pixel values
+    if len(mode) > 1:
+        #multiband image has extrema for each band
+        depth = 0
         for i in range(len(extra)):
+            #get the max range out of all bands
             depth = max(extra[i][1] - extra[i][0], depth)
-            dmin = min(extra[i][0], dmin)
-            dmax = max(extra[i][1], dmax)
     else:
-        #no transparency layer
+        #single band image
+        depth = extra[1] - extra[0]
         if mode == '1':
-            #1-bit pixel case, range of 0-1
+            #1-bit mode, range of 0-1
             full = 1
-        dmin = extra[0]
-        dmax = extra[1]
-        depth = dmax - dmin
     if depth != full:
-        return True, dmin, dmax
-    return False, dmin, dmax
+        #b&w images should use full pixel range
+        return True
+    return False
 
-def scale_pixels(myImage, dmin, dmax):
-    #scale problematic 1-bit and 8-bit pixel depth to full range
-    #For 1-bit images, this rescales pixels if they exceed allowed pixel values
-    #we're presuming the pixel values are binary
-    full = 255
-    if myImage.mode == '1':
-        full = 1
-    data = myImage.getdata()
-    myImage.putdata(data, full/dmax, -dmin)
+
+
+def fix_palette(myImage):
+    #fix problematic P mode images
+    #I found it wasn't necessary to fix the 1-bits
+    img_pal = myImage.getpalette()
+    for i in range(0, 3):
+        img_pal[i] = 255
+    for i in range(3, 6):
+        img_pal[i] = 0
+    myImage.putpalette(img_pal)
     return myImage
 
-def is_transparent(myImage):
-    #If there is no Alpha 'A' channel, check for transp. info
-    if 'A' not in myImage.mode:
-        if myImage.info.get('transparency', None) is not None:
-            return True
-        else:
-            return False
-    return True
 
-def add_mask(filename, myImage):
-    #paste image onto black or white background
+def process_trans(filename, myImage):
     #expects files named to indicate b or w color
-    #couldn't get composite to work with LA mode, so it's in RGBA for now
+    alpha = myImage.getchannel('A')
     if(re.search(r"black", filename)):
-        #icon should be black, mask with white
-        white_mask = Image.new("RGBA", myImage.size, (255,255,255))
-        myImage = Image.alpha_composite(white_mask, myImage)
+        #icon should be black
+        myImage = ImageOps.invert(alpha)
     if(re.search(r"white", file)):
-        #icon is white, mask with black
-        black_mask = Image.new("RGBA", myImage.size, (0,0,0))
-        myImage = Image.alpha_composite(black_mask, myImage)
+        #icon is white
+        myImage = alpha
     return myImage
 
 
@@ -77,18 +62,30 @@ in_dir = '../images/'
 out_dir = '../processed/'
 pattern = 'ic*dp'
 size = (128, 128)
+flagged = []
+count = 0
 
 #make output dir if does not exist
 pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 file_list = [path.abspath(x) for x in glob(in_dir + pattern)]
 for file in file_list:
-    im = Image.open(file)
-    out_im = im.resize(size).rotate(270)
-    bad_depth, dmin, dmax = has_bad_extrema(out_im)
-    if bad_depth:
-        #scale pixels simply to span their mode depth
-        out_im = scale_pixels(out_im, dmin, dmax)
-    elif is_transparent(im):
-        out_im = add_mask(file, out_im.convert('RGBA'))
-    out_im = out_im.convert('L').save(out_dir + path.basename(file), 'jpeg')
+    print(file)
+    img = Image.open(file)
+    out_img = img.resize(size).rotate(270)
+    bad_depth = has_bad_extrema(out_img)
+    if bad_depth: #these will be my 'P' and '1' mode imgs
+        #ignore '1' modes that were flagged, fix 'P' ones
+        if out_img.mode == 'P':
+            out_img = fix_palette(out_img)
+        flagged.append(path.basename(file))
+        count += 1
+    elif img.mode == 'LA':
+        out_img = process_trans(file, out_img)
+        count += 1
+    else:
+        print(f'Your script missed some image:\n{file}')
+    out_img = out_img.convert('L').save(out_dir + path.basename(file), 'jpeg')
+
+print(f'{count} images done processing.\n  The following images were flagged during processing:\n')
+print("\n".join(flagged))
